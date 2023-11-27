33a6bc09072e2d5ed4eb7f0a56e56923283fcb6f
Sebastian Scherbes <github@scherbes.de>
2023-11-27 17:08:10 +0700

    refactor form component

--- app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -1,10 +1,12 @@
+/** @format */
+
 "use client";
 
 import { createBoard } from "@/actions/create-board";
-import { useFormState } from "react-dom";
-import { FormInput } from "./form-input";
-import { FormButton } from "./form-button";
+
 import { useAction } from "@/hooks/use-action";
+import { FormInput } from "@/components/form/form-input";
+import { FormSubmit } from "@/components/form/form-submit";
 
 export const Form = () => {
   const { execute, fieldErrors } = useAction(createBoard, {
@@ -14,6 +16,7 @@ export const Form = () => {
 
   const onSubmit = (formData: FormData) => {
     const title = formData.get("title") as string;
+    console.log({ title });
 
     execute({ title });
   };
@@ -21,9 +24,13 @@ export const Form = () => {
   return (
     <form action={onSubmit}>
       <div className="flex flex-col space-y-2">
-        <FormInput errors={fieldErrors} />
+        <FormInput
+          id="title"
+          label="Board Title"
+          errors={fieldErrors}
+        />
       </div>
-      <FormButton />
+      <FormSubmit>Save</FormSubmit>
     </form>
   );
 };
