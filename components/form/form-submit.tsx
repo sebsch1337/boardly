ba513f1d3d094bed2996c6f74b516722fbe44273
Sebastian Scherbes <github@scherbes.de>
2023-11-29 11:21:06 +0700

    set primary as default button variant

--- components/form/form-submit.tsx
+++ components/form/form-submit.tsx
@@ -7,24 +7,24 @@ import { Button } from "../ui/button";
 import { cn } from "@/lib/utils";
 
 interface FormSubmitProps {
-  children: React.ReactNode;
-  disabled?: boolean;
-  className?: string;
-  variant?: "default" | "destructive" | "outline" | "secondary" | "ghost" | "link" | "primary";
+	children: React.ReactNode;
+	disabled?: boolean;
+	className?: string;
+	variant?: "default" | "destructive" | "outline" | "secondary" | "ghost" | "link" | "primary";
 }
 
-export const FormSubmit = ({ children, disabled, className, variant }: FormSubmitProps) => {
-  const { pending } = useFormStatus();
+export const FormSubmit = ({ children, disabled, className, variant = "primary" }: FormSubmitProps) => {
+	const { pending } = useFormStatus();
 
-  return (
-    <Button
-      disabled={pending || disabled}
-      type="submit"
-      variant={variant}
-      size="sm"
-      className={cn(className)}
-    >
-      {children}
-    </Button>
-  );
+	return (
+		<Button
+			disabled={pending || disabled}
+			type="submit"
+			variant={variant}
+			size="sm"
+			className={cn(className)}
+		>
+			{children}
+		</Button>
+	);
 };
