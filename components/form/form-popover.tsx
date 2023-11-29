83f4f03d86a4884d3f8c6290c46da8aa81f38b01
Sebastian Scherbes <github@scherbes.de>
2023-11-29 11:20:49 +0700

    add form picker to popover component

--- components/form/form-popover.tsx
+++ components/form/form-popover.tsx
@@ -8,6 +8,9 @@ import { FormSubmit } from "./form-submit";
 import { useAction } from "@/hooks/use-action";
 import { createBoard } from "@/actions/create-board";
 import { toast } from "sonner";
+import { FormPicker } from "./form-picker";
+import { ElementRef, useRef } from "react";
+import { useRouter } from "next/navigation";
 
 interface FormPopoverProps {
 	children: React.ReactNode;
@@ -17,21 +20,25 @@ interface FormPopoverProps {
 }
 
 export const FormPopover = ({ children, side = "bottom", align, sideOffset = 0 }: FormPopoverProps) => {
+	const router = useRouter();
+	const closeRef = useRef<ElementRef<"button">>(null);
+
 	const { execute, fieldErrors } = useAction(createBoard, {
 		onSuccess: (data) => {
-			console.log({ data });
 			toast.success("Board created!");
+			closeRef.current?.click();
+			router.push(`/board/${data.id}`);
 		},
 		onError: (error) => {
-			console.log({ error });
 			toast.error(error);
 		},
 	});
 
 	const onSubmit = (formData: FormData) => {
 		const title = formData.get("title") as string;
+		const image = formData.get("image") as string;
 
-		execute({ title });
+		execute({ title, image });
 	};
 
 	return (
@@ -44,7 +51,10 @@ export const FormPopover = ({ children, side = "bottom", align, sideOffset = 0 }
 				sideOffset={sideOffset}
 			>
 				<div className="text-sm font-medium text-center text-neutral-600 pb-4">Create board</div>
-				<PopoverClose asChild>
+				<PopoverClose
+					ref={closeRef}
+					asChild
+				>
 					<Button
 						className="h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600"
 						variant="ghost"
@@ -57,6 +67,10 @@ export const FormPopover = ({ children, side = "bottom", align, sideOffset = 0 }
 					className="space-y-4 "
 				>
 					<div className="space-y-4">
+						<FormPicker
+							id="image"
+							errors={fieldErrors}
+						/>
 						<FormInput
 							id="title"
 							label="Board title"
