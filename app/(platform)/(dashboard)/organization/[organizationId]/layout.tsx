573b2547f527ad493a71efa4086baac6fe56d8db
Sebastian Scherbes <github@scherbes.de>
2023-11-30 17:57:10 +0700

    create organizationId layout

--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,12 +1,21 @@
+import { auth } from "@clerk/nextjs";
+import { startCase } from "lodash";
+
 import { OrgControl } from "./_components/org-control";
 
+export async function generateMetadata() {
+	const { orgSlug } = auth();
+
+	return { title: startCase(orgSlug || "organization") };
+}
+
 const OrganizationIdLayout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <>
-      <OrgControl />
-      {children}
-    </>
-  );
+	return (
+		<>
+			<OrgControl />
+			{children}
+		</>
+	);
 };
 
 export default OrganizationIdLayout;
