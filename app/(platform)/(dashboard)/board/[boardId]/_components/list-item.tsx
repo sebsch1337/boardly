3536c6fa1c99e127e62109a7ae2ca591e6564e13
Sebastian Scherbes <github@scherbes.de>
2023-12-06 12:52:52 +0900

    show CardItems on ListItem component

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
@@ -6,6 +6,8 @@ import { ListWithCards } from "@/types";
 
 import { ListHeader } from "./list-header";
 import { CardForm } from "./card-form";
+import { cn } from "@/lib/utils";
+import { CardItem } from "./card-item";
 
 interface ListItemProps {
 	data: ListWithCards;
@@ -35,6 +37,15 @@ export const ListItem = ({ data, index }: ListItemProps) => {
 					onAddCard={enableEditing}
 					data={data}
 				/>
+				<ol className={cn("mx-1 px-1 py-0.5 flex flex-col gap-y-2", data.cards.length > 0 ? "mt-2" : "mt-0")}>
+					{data.cards.map((card, index) => (
+						<CardItem
+							index={index}
+							key={card.id}
+							data={card}
+						/>
+					))}
+				</ol>
 				<CardForm
 					listId={data.id}
 					ref={textareaRef}
