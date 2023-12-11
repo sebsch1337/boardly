79be9d4369d29e11173df87ad272260d44333ec8
Sebastian Scherbes <github@scherbes.de>
2023-12-11 18:04:46 +0900

    add pro text to organization page

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -11,6 +11,7 @@ import { MAX_FREE_BOARDS } from "@/constants/board";
 import { getAvailableCount } from "@/lib/org-limit";
 
 import { HelpCircle, User2 } from "lucide-react";
+import { checkSubscription } from "@/lib/subscription";
 
 export const BoardList = async () => {
 	const { orgId } = auth();
@@ -27,6 +28,7 @@ export const BoardList = async () => {
 	});
 
 	const availableCount = await getAvailableCount();
+	const isPro = await checkSubscription();
 
 	return (
 		<div className="space-y-4">
@@ -54,7 +56,7 @@ export const BoardList = async () => {
 						className="aspect-video relative h-full w-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition"
 					>
 						<p className="text-sm">Create new board</p>
-						<span className="text-xs">{`${MAX_FREE_BOARDS - availableCount} remaining`}</span>
+						<span className="text-xs">{isPro ? "Unlimited" : `${MAX_FREE_BOARDS - availableCount} remaining`}</span>
 						<Hint
 							sideOffset={40}
 							description={`Free workspaces can have up to 5 open boards. For unlimited boards upgrade this workspace.`}
